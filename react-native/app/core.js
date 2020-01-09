2a4d45c6f5d9f6146aede4d3779af1ca898647e6
Eric Ihli <ericihli@gmail.com>
2020-01-09 13:36:11 -0800

    Remove compiled files from git

--- react-native/app/core.js
+++
@@ -1,7 +0,0 @@
-goog.provide('example.core');
-goog.require('cljs.core');
-example.core.init = (function example$core$init(){
-return cljs.core.print.cljs$core$IFn$_invoke$arity$variadic(cljs.core.prim_seq.cljs$core$IFn$_invoke$arity$2(["Init"], 0));
-});
-
-//# sourceMappingURL=core.js.map
--- react-native/app/shadow.js.shim.module$react.js
+++
@@ -1,6 +0,0 @@
-goog.provide("shadow.js.shim.module$react");
-goog.provide("module$shadow_js_shim_module$react");
-shadow.js.shim.module$react = require("react");
-module$shadow_js_shim_module$react.default = shadow.js.shim.module$react;
-
-//# sourceMappingURL=shadow.js.shim.module$react.js.map
