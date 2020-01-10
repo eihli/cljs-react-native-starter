8db7eefd194acb79bdfd31e3e72221df646ffafb
Eric Ihli <ericihli@gmail.com>
2020-01-10 15:23:08 -0800

    Add stack navigation

--- src/example/core.cljs
+++ src/example/core.cljs
@@ -30,13 +30,26 @@
 (defn home
   []
   [:> rn/View
-   [:> rn/Text "Hello worlds!"]
+   [:> rn/Text "Hello world!"]
    [child]])
 
-(defn tab-1
+(defn new-item
   []
   [:> rn/View
-   [:> rn/Text "This is a tab"]])
+   [:> rn/Text "Create new thing"]])
+
+(defn edit-item
+  []
+  [:> rn/View
+   [:> rn/Text "Edit existing thing"]])
+
+(defn settings
+  []
+  [:> rn/View
+   [:> rn/Text "Settings Tab"]
+   [:> rn/TouchableHighlight
+    [new-item]] 
+   [edit-item]])
 
 (defonce nav-state (atom nil))
 
@@ -48,36 +61,56 @@
   (js/Promise. (fn [resolve]
                  (resolve @nav-state))))
 
+;; navigationOptions needs to be set on the react
+;; class as a static method. That's why we
+;; reactify it in the let above and use this
+;; doto with goog.object/set to give home that
+;; property.
+;;
+;; The reason for all of this is that we want
+;; to override the default behavior of the
+;; tab bar click handler. By default, it updates
+;; the state of components, it sets state in some props
+;; that handles displaying different screens.
+;;
+;; In re-frame fashion, we want all state to be handled
+;; in events and stored in the app-db. This way,
+;; while we're editing and hot-reloading stuff, we
+;; don't lose the state that we're in. If we're working
+;; on a screen that's nested deep down in our route,
+;; we don't want a hot-reload to bump us back up to the
+;; home screen, which is what would happen if
+;; state was stored in the props of components.
+
+(def home-stack
+  (create-stack-navigator
+   (clj->js
+    {:home/home (doto (reagent/reactify-component home)
+                  (goog.object/set
+                   "navigationOptions"
+                   #js {:title "Home"}))})))
+
+(def settings-stack
+  (create-stack-navigator
+   (clj->js
+    {:settings/home (doto (reagent/reactify-component settings)
+                      (goog.object/set
+                       "navigationOptions"
+                       #js {:title "Settings"}))})))
+
 (def tab-navigator
   (create-bottom-tab-navigator
-   #js {:home (let [home (reagent/reactify-component home)]
-                ;; navigationOptions needs to be set on the react
-                ;; class as a static method. That's why we
-                ;; reactify it in the let above and use this
-                ;; doto with goog.object/set to give home that
-                ;; property.
-                ;;
-                ;; The reason for all of this is that we want
-                ;; to override the default behavior of the
-                ;; tab bar click handler. By default, it updates
-                ;; the state of components, it sets state in some props
-                ;; that handles displaying different screens.
-                ;;
-                ;; In re-frame fashion, we want all state to be handled
-                ;; in events and stored in the app-db. This way,
-                ;; while we're editing and hot-reloading stuff, we
-                ;; don't lose the state that we're in. If we're working
-                ;; on a screen that's nested deep down in our route,
-                ;; we don't want a hot-reload to bump us back up to the
-                ;; home screen, which is what would happen if
-                ;; state was stored in the props of components.
-                (doto home
-                  (goog.object/set "navigationOptions"
-                                   #js {:tabBarOnPress identity})))
-        :tab-1 (let [tab-1 (reagent/reactify-component tab-1)]
-                 (doto tab-1
-                   (goog.object/set "navigationOptions"
-                                    #js {:tabBarOnPress identity})))}))
+   #js {:home
+        (doto home-stack
+          (goog.object/set "navigationOptions"
+                           #js {;; :tabBarOnPress identity
+                                :tabBarLabel "Home"}))
+        
+        :settings
+        (doto settings-stack
+          (goog.object/set "navigationOptions"
+                           #js {;; :tabBarOnPress identity
+                                :tabBarLabel "Settings"}))})) 
 
 (def navigator-ref (atom nil))
 
