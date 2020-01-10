77003b94261d08b0472c5bc01c4e03c72b3158f6
Eric Ihli <ericihli@gmail.com>
2020-01-09 20:25:13 -0800

    Simplify reloader

--- src/example/core.cljs
+++ src/example/core.cljs
@@ -21,35 +21,24 @@
    [child]])
 
 (defn make-reloader [comp]
-  (let [comp-ref (atom comp)
+  (let [comp-ref (reagent/atom comp)
         wrapper-ref (atom nil)
         wrapper (reagent/create-class
                  {:render
                   (fn []
                     (let [comp @comp-ref]
-                      (if (fn? comp) (comp) comp)))
-                  
-                  :component-did-mount
-                  (fn []
-                    (this-as this
-                      (reset! wrapper-ref this)))})]
+                      (if (fn? comp) (comp) comp)))})]
     ;; Whatever we register with rn/AppRegistry must exist
     ;; permanently. Since we can't change it, we must treat it
     ;; like a wrapper and wrap something that we *can* change.
     ;;
     ;; The `wrapper` component we created above renders whatever
     ;; is in our `comp-ref` atom. We get hot reloading
-    ;; by resetting the value in that atom and forcing
-    ;; a re-render with `forceUpdate`.
+    ;; by resetting the value in that atom. Since it's a
+    ;; reagent/atom, changing it forces a re-render.
     (rn/AppRegistry.registerComponent "ExampleApp" (fn [] wrapper))
     (fn [comp]
-      (when-let [wrapper @wrapper-ref]
-        ;; Why do we need to reset the component here?
-        ;; Why doesn't calling forceUpdate on the parent-most component
-        ;; cause everything to re-render?
-        ;; Is there a downside to doing it this way? Is there an alternative?
-        (reset! comp-ref comp)
-        (.forceUpdate @wrapper-ref)))))
+      (reset! comp-ref comp))))
 
 (defonce reload (make-reloader home))
 
